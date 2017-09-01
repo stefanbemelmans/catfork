c059b171aca8470c0970283cc935797a3f451bcb
stackbaxter <stackbaxter@gmail.com>
2017-09-01 02:59:34 -0500

    on the way

---
+++ src/components/Recipe.js
@@ -0,0 +1,20 @@
+export default function Recipe (props) {
+
+    return (
+      <div className="recipe" key={props.rec.id}>
+        <button onClick={() => this.getIndRec(rec.id)}>
+          {" "}{props.rec.title}{" "}
+        </button>
+        <img
+          className="recPic"
+          src={props.rec.image}
+          alt="../images/CatforkTitleSmall.png"
+        />
+        {this.state.instructions[rec.id]
+          ? this.renderInstructions(this.state.instructions[props.rec.id])
+          : ""}
+      </div>
+    );
+  });
+}
+}
