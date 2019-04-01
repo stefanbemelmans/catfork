0f29a3229be8f992364631b3ee30b351a11c11b0
stackbaxter <stackbaxter@yahoo.com>
2019-04-01 13:12:55 -0500

    lots a stuff

--- src/components/CatPicComponent.js
+++ src/components/CatPicComponent.js
@@ -1,5 +1,5 @@
   import React, {Component} from 'react';
-
+  import axios from "axios";
   export default  class CatPic extends React.Component {
     constructor() {
       super();
@@ -11,7 +11,7 @@
   
     resetCatPic() {
       let newCat = "https://thecatapi.com/api/images/get?format=src&size=med";
-      fetch(newCat)
+      axios(newCat)
         .then(function(response) {
           console.log(response);
           return response.blob();
@@ -27,7 +27,7 @@
   
     render() {
       return (
-        <div Col-xs={10} Col-sm={8} Col-md={6}>
+        <div Col-xs-10 Col-sm-8 Col-md-6>
           <a href="https://thecatapi.com">
             <img className="catPic" src={this.state.kitty} alt="logo" />
           </a>
