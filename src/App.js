e0fe3baf976b8ac3a59cfa8e792b6904a48bac8e
Stefan L. Bemelmans <stefanlbemelmans@gmail.com>
2019-11-04 06:58:56 +0000

    minor major adjustments, refactoring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-undef */
 import React, { Component } from 'react';
 import catForkImg from "./images/catForkImg_square.png";
-import CatPic from "./components/CatPicComponent.js";
+import { CatPic } from "./components/CatPicComponent.js";
 import "./App.css";
 import SearchBox from "./containers/SearchBoxContainer";
 import RecipeList from "./containers/RecipeListContainer";
@@ -15,6 +16,7 @@ class App extends Component {
 
  async resetCatPic() {
    var response = await fetch("https://thecatapi.com/api/images/get?format=src&size=med")
+   // eslint-disable-next-line no-console
    console.log("hitting getnew cat", response);
    this.setState({
      catUrl: response.url
@@ -26,7 +28,6 @@ class App extends Component {
       <div className="app">
         <div className="app-header">
           <img src={catForkImg} className="" alt="logo" />
-          
         </div>
         <p className="app-intro"> This is a recipe search app based on ingredients you enter. Please separate them by a comma and enter at least two (2). </p>
         <CatPic catUrl={this.state.catUrl} />
--- src/components/CatPicComponent.js
+++ src/components/CatPicComponent.js
@@ -1,20 +1,13 @@
-  import React, {Component} from 'react';
+  import React from 'react';
   // import axios from "axios";
-  export default  class CatPic extends Component {
-    constructor(props) {
-      super(props);
-    }
-  
-    render() {
+  export const CatPic = (props) => {
       return (
         <div>
           <a href="https://thecatapi.com">
-            <img className="catPic" src={this.props.catUrl} alt="logo" />
+            <img className="catPic" src={props.catUrl} alt="logo" />
           </a>
-          <button className="btn-class" onClick={this.resetCatPic}>
-            New Cat
-          </button>
         </div>
       );
-    }
-  }
+    };
+    
+  
