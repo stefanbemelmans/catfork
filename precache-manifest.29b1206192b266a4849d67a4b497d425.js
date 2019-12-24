4816e4fc9f01bc2a57c22c743bf4b3e099ddc64a
Stefan L. Bemelmans <stefanlbemelmans@gmail.com>
2019-12-24 17:30:05 -0500

    Updates

---
+++ precache-manifest.29b1206192b266a4849d67a4b497d425.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba64fa63993a46f1506f19a52935603",
+    "url": "/catfork/index.html"
+  },
+  {
+    "revision": "9263fc07288c7391d903",
+    "url": "/catfork/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "94287030b840d595b5e3",
+    "url": "/catfork/static/css/main.71844ba9.chunk.css"
+  },
+  {
+    "revision": "9263fc07288c7391d903",
+    "url": "/catfork/static/js/2.667f06fe.chunk.js"
+  },
+  {
+    "revision": "94287030b840d595b5e3",
+    "url": "/catfork/static/js/main.1e999162.chunk.js"
+  },
+  {
+    "revision": "6b6c041132f1163dce3f",
+    "url": "/catfork/static/js/runtime-main.a0751c17.js"
+  },
+  {
+    "revision": "5d6f0df54554aaa1d898ad3f01160eef",
+    "url": "/catfork/static/media/CatForkLogo.5d6f0df5.png"
+  }
+]);
