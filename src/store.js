c397a6c76e7d010eb0f46a7928ba7bf9a3066c90
Stefan L. Bemelmans <stefanlbemelmans@gmail.com>
2019-12-24 14:06:12 -0500

    lots of cleaning

--- src/store.js
+++ src/store.js
@@ -1,15 +1,12 @@
 import recipes from "./Features/Recipes/recipeReducers";
 import {createStore, applyMiddleware, compose} from "redux";
-import thunkMiddleware from "redux-thunk";
 import loggerMiddleware from './middleware/logger'
 import monitorReducerEnhancer from './enhancers/monitorReducer'
 
-const middlewareEnhancer = applyMiddleware(loggerMiddleware, thunkMiddleware)
+const middlewareEnhancer = applyMiddleware(loggerMiddleware)
 const composedEnhancers = compose(
   middlewareEnhancer,
   monitorReducerEnhancer
 )
-const store = createStore(recipes,undefined, composedEnhancers, 
-  // eslint-disable-next-line no-undef
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(recipes,undefined, composedEnhancers)
 export default store;
