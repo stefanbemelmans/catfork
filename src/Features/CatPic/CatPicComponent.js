ec9460244a8fb665651f76ee1c780e18bce19114
Stefan L. Bemelmans <stefanlbemelmans@gmail.com>
2020-01-14 01:05:39 -0500

    new cat url

--- src/Features/CatPic/CatPicComponent.js
+++ src/Features/CatPic/CatPicComponent.js
@@ -8,14 +8,14 @@ import Axios from "axios";
 const dotenv = require("dotenv");
 dotenv.config();
 
-const serverCatString = "https://shielded-eyrie-66484.herokuapp.com/api/getCat"
-
+// const serverCatString = "https://shielded-eyrie-66484.herokuapp.com/api/getCat"
+const serverCatString = "https://servercat.herokuapp.com/api/getCat"
 export const CatPicComponent = () => {
   const [catPicUrl, setCatPicUrl] = useState("");
 
   useEffect(() => {
     if (!catPicUrl) {
-      console.log("setting CatPic in UseEffect");
+      // console.log("setting CatPic in UseEffect");
       getCatPic(setCatPicUrl);
     }
   });
@@ -24,7 +24,7 @@ export const CatPicComponent = () => {
     <Container className="mb-1">
       <Row className="catPicComponent">
         <Col xs={12} sm={6} className="text-center">
-          <Image src={catPicUrl} alt="ServerCat is sending a cat... wait for it..." className="m-1 catPic" />
+          <Image src={catPicUrl} alt="serverCat is sending a cat... wait for it..." className="m-1 catPic" />
         </Col>
         <Col xs={12} sm={6} className="text-center">
           <button className="btn-class" onClick={() => getCatPic(setCatPicUrl)}>
@@ -39,6 +39,6 @@ export const CatPicComponent = () => {
 
 async function getCatPic(setCatPicUrl) {
   var response = await Axios(serverCatString)
-  console.log(response, "response from serverCat")
+  // console.log(response, "response from serverCat")
   setCatPicUrl(response.data);
 }
