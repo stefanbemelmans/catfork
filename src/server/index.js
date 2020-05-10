64018d472af9f7d72e286255bca5d6b1e9689594
stackbaxter <stackbaxter@yahoo.com>
2020-05-10 13:47:32 -0400

    adjustments

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,4 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
+// var express = require('express');
+// var app = express();
+// app.use(express.static(__dirname + '/'));
+// app.listen(process.env.PORT || 8080);
